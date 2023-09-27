611863faef4c22424d6999bf7ca904929b8b93dd
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2023-09-27 09:36:32 +0300

    Update ApplyJScript.cs

--- ApplyJScript.cs
+++ ApplyJScript.cs
@@ -1,8 +1,27 @@
-public void ApplyJScriptFix()
-{
-    var xmlSerializerType = typeof(XmlSerializer);
-    const BindingFlags bindingAttr = BindingFlags.Default | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static;
+using System;
+using System.Reflection;
+using System.Xml.Serialization;
 
-    var method = xmlSerializerType.GetMethod("set_Mode", bindingAttr);
-    method?.Invoke(null, new object[] { 1 });
+public class XmlSerializerHelper
+{
+    public static void SetXmlSerializerMode()
+    {
+        // XmlSerializer türünü alın
+        Type xmlSerializerType = typeof(XmlSerializer);
+
+        // XmlSerializer türündeki "set_Mode" adlı özel bir static metodu bulun
+        const BindingFlags bindingAttr = BindingFlags.Static | BindingFlags.NonPublic;
+        MethodInfo setModeMethod = xmlSerializerType.GetMethod("set_Mode", bindingAttr);
+
+        if (setModeMethod != null)
+        {
+            // "set_Mode" metodunu çağırarak XmlSerializer modunu ayarlayın
+            setModeMethod.Invoke(null, new object[] { 1 });
+        }
+        else
+        {
+            // "set_Mode" metodu bulunamazsa uygun bir hata işleme stratejisi uygulanabilir.
+            Console.WriteLine("Hata: 'set_Mode' metodu bulunamadı.");
+        }
+    }
 }
